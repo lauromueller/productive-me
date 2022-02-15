46775b8278af0f7231eca9c9a350a4a5302f8240
YouTube <youtube@Lauros-MacBook-Pro.local>
2022-02-15 19:00:00 +0100

    feat: add static analysis and pre commit hooks

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-    <div>This is an awesome new react app</div>,
-    document.getElementById('root')
-);
+ReactDOM.render(<div>This is an awesome new react app</div>, document.getElementById('root'));
